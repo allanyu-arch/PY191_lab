f3b10853e4e0a96a4608b7123ae12eadc1a16f8e
PaperTP <dbdbdb5082@gmail.com>
2026-07-07 13:43:04 -0400

    wtf

---
+++ notebooks/Week2Day1_lab.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+station_df = pd.read_csv("notebooks/station_weather.csv")
+station_df.head()
+# locations = data_df[""]
+# temperatures = data_df[data_df[""]]
+# fig, ax = plt.subplot(figsize=(7,3)) #change
